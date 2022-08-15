7d6fb34495aa9bd5c34eab3dddb3089a1fa6328d
Ash <49338180+mishap-00@users.noreply.github.com>
2022-08-15 17:53:22 +0100

    Add files via upload

---
+++ Assets/Scenes/BoidManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidManager : MonoBehaviour {
+    public static BoidManager Instance;
+
+    [SerializeField] private int boidAmount;
+    [SerializeField] private GameObject boidPrefab;
+
+    public float boidSpeed;
+    public float boidPerceptionRadius;
+    public float cageSize;
+
+    public float separationWeight;
+    public float cohesionWeight;
+    public float alignmentWeight;
+
+    public float avoidWallsWeight;
+    public float avoidWallsTurnDist;
+
+    public List<BoidV1> boids;
+
+    /// <summary>
+    /// The initial method called during the run cycle of the program. 
+    /// The Awake method is responsible for creating the boids and putting them in a list.
+    /// These boids then run through their update cycles
+    /// </summary>
+    private void Awake() {
+
+        Instance = this;
+        boids.Clear();
+
+        for (int i = 0; i < boidAmount; i++) {
+            Vector3 pos = new Vector3(
+                Random.Range(-cageSize / 2f, cageSize / 2f),
+                Random.Range(-cageSize / 2f, cageSize / 2f),
+                Random.Range(-cageSize / 2f, cageSize / 2f)
+            );
+            Quaternion rot = Quaternion.Euler(
+                Random.Range(0f, 360f),
+                Random.Range(0f, 360f),
+                Random.Range(0f, 360f)
+            );
+
+            BoidV1 newBoid = Instantiate(boidPrefab, pos, rot).GetComponent<BoidV1>();
+            boids.Add(newBoid);
+        }
+    }
+
+    public List<BoidV1> GetBoids() { return boids; }
+}
---
+++ Assets/Scenes/BoidV1.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+public class BoidV1 : MonoBehaviour {
+
+    private BoidManager manager;
+
+    private Vector3 separationForce;
+    private Vector3 cohesionForce;
+    private Vector3 alignmentForce;
+
+    private Vector3 avoidWallsForce;
+
+    private void Start() {
+        manager = BoidManager.Instance;
+    }
+
+    /// <summary>
+    /// The basic update method, this loops each frame and calls the calculation for each boids next movement and then moves them
+    /// </summary>
+    private void Update() {
+        calculateForces();
+        moveForward();
+    }
+
+    /// <summary>
+    /// The calculateForces method calculates each boids next movement based on the positions of each boid in the scene as well as the cage they're in
+    /// </summary>
+    private void calculateForces() {
+
+        Vector3 seperationSum = Vector3.zero;
+        Vector3 positionSum = Vector3.zero;
+        Vector3 headingSum = Vector3.zero;
+
+        int boidsNearby = 0;
+
+        for (int i = 0; i < manager.boids.Count; i++) {
+
+            if (this != manager.boids[i]) {
+
+                Vector3 otherBoidPosition = manager.boids[i].transform.position;
+                float distToOtherBoid = (transform.position - otherBoidPosition).magnitude;
+
+                if (distToOtherBoid < manager.boidPerceptionRadius) {
+
+                    seperationSum += -(otherBoidPosition - transform.position) * (1f / Mathf.Max(distToOtherBoid, .0001f));
+                    positionSum += otherBoidPosition;
+                    headingSum += manager.boids[i].transform.forward;
+
+                    boidsNearby++;
+                }
+            }
+        }
+
+        if (boidsNearby > 0) {
+            separationForce = seperationSum / boidsNearby;
+            cohesionForce   = (positionSum / boidsNearby) - transform.position;
+            alignmentForce  = headingSum / boidsNearby;
+        }
+        else {
+            separationForce = Vector3.zero;
+            cohesionForce   = Vector3.zero;
+            alignmentForce  = Vector3.zero;
+        }
+
+    	if (minDistToBorder(transform.position, manager.cageSize) < manager.avoidWallsTurnDist) {
+            // Back to center of cage
+            avoidWallsForce = -transform.position.normalized;
+        }
+        else {
+            avoidWallsForce = Vector3.zero;
+        }
+    }
+    
+    /// <summary>
+    /// The moveForward method implements the movement decided by the calculate forces method to travel the correct distance from its current position
+    /// </summary>
+    private void moveForward() {
+        Vector3 force = 
+            separationForce * manager.separationWeight +
+            cohesionForce   * manager.cohesionWeight +
+            alignmentForce  * manager.alignmentWeight +
+            avoidWallsForce * manager.avoidWallsWeight;
+
+        Vector3 velocity = transform.forward * manager.boidSpeed;
+        velocity += force * Time.deltaTime;
+        velocity = velocity.normalized * manager.boidSpeed;
+
+        transform.position += velocity * Time.deltaTime;
+        transform.rotation = Quaternion.LookRotation(velocity);
+    }
+
+    private float minDistToBorder(Vector3 pos, float cageSize) {
+        float halfCageSize = cageSize / 2f;
+        return Mathf.Min(Mathf.Min(
+            halfCageSize - Mathf.Abs(pos.x),
+            halfCageSize - Mathf.Abs(pos.y)),
+            halfCageSize - Mathf.Abs(pos.z)
+        );
+    }
+}
